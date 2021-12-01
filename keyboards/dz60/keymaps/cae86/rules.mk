7c55deabaa075f28436339ccf6b41b05902fc351
ceybers <95356162+ceybers@users.noreply.github.com>
2021-12-01 18:23:14 +0200

    Create rules.mk

---
+++ keyboards/dz60/keymaps/cae86/rules.mk
@@ -0,0 +1,22 @@
+TAP_DANCE_ENABLE  = no
+LEADER_ENABLE     = no
+AUTO_SHIFT_ENABLE = yes
+
+BOOTMAGIC_ENABLE  = no      # Virtual DIP switch configuration
+MOUSEKEY_ENABLE   = no      # Mouse keys
+EXTRAKEY_ENABLE   = yes     # Audio control and System control
+CONSOLE_ENABLE    = no      # Console for debug
+COMMAND_ENABLE    = no      # Commands for debug and configuration
+# Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
+SLEEP_LED_ENABLE  = no      # Breathing sleep LED during USB suspend
+NKRO_ENABLE       = no      # USB Nkey Rollover
+BACKLIGHT_ENABLE  = yes     # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE   = yes     # Enable keyboard RGB underglow
+BLUETOOTH_ENABLE  = no      # Enable Bluetooth
+AUDIO_ENABLE      = no      # Audio output
+
+LAYOUTS = LAYOUT_directional
+
+LT0_ENABLE         = no
+
+DYNAMIC_MACRO_ENABLE = yes
